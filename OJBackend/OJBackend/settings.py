32a6194cfe6090ce370252a75a49f1b5d2128fbd
segupavankumar <62418622+segupavankumar@users.noreply.github.com>
2022-07-22 18:19:28 +0530

    Update settings.py

--- OJBackend/OJBackend/settings.py
+++ OJBackend/OJBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jh^#me(eetsa-%80j(*xd0f6_ls-mu_x@9-*65@v98p%)z+h9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
